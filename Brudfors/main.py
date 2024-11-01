b68e661187c5fcc6d99ea2c3a4698a56e3ed2b0e
acewol <147809921+acewol@users.noreply.github.com>
2024-11-01 17:09:52 +0300

    Comments

---
+++ Brudfors/main.py
@@ -0,0 +1,18 @@
+import itertools
+import string
+
+def brute_password(password, min_len, max_len): #Указывание минимальное кол-во символов и максимальное
+    chars = string.ascii_letters + string.digits #Алфавит и цифры. По которым будет подбор.
+    atempt = 0
+
+    for lenght in range(min_len, max_len):
+        for guess in itertools.product(chars, repeat=lenght):
+            atempt += 1
+            guess = ''.join(guess)
+            print(f"Попытка №{atempt}. {guess}")
+            if guess == password:
+                print("Пароль найден")
+                return
+
+password = '1234'
+brute_password(password, 1, 5) #Можно поставить нужное кол-во символов в пароле
